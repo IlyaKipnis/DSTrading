65a31088a815b8a0da82471b18bff9f271ab98f7
Ilya Kipnis <ikipnis@r-server.(none)>
2014-04-12 22:16:41 +0000

    Adding VIDA.R and updating help files.

---
+++ R/VIDA.R
@@ -0,0 +1,28 @@
+cppFunction('
+  NumericVector computeVIDA(NumericVector k, NumericVector price, 
+    NumericVector filt, NumericVector consts){
+            int n = price.size();
+            for(int i=1; i<n; i++) {
+            filt[i] = consts[i]*k[i]*price[i]+(1-consts[i]*k[i])*filt[i-1];
+            }
+            return filt;
+            }
+            ')
+#'Variable Index Dynamic Average
+#'@param x -- a price series
+#'@param n -- a short period number over which to take a running SD
+#'@param m -- a long period number over which to take a running SD
+#'@param smoothConstant -- a parameter that affects smoothing. k*p(t) + (1-k)*p(t-1)
+#'@return -- a stream of Variable Index Dynamic Average computations.
+#'@references 
+#'\cr \url{"http://www.mesasoftware.com/Seminars/TradeStation\%20World\%2005.pdf"} \cr
+#'@export
+"VIDA" <- function(x, n=9, m=30, smoothConstant=.2, ...) {
+  k <- runSD(x,n=n)/runSD(x,n=m)
+  k[is.na(k)] <- 0
+  filt <- rep(0, length(x))
+  filt[1] <- x[1]
+  consts <- rep(smoothConstant, length(x))
+  out <- computeVIDA(k, x, filt, consts)
+  out <- xts(out, order.by=index(x))
+}

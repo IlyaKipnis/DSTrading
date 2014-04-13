98b1131cddf17a62c4929e70bcc699af01e0c441
Ilya Kipnis <ikipnis@r-server.(none)>
2014-04-13 01:22:40 +0000

    "Fixing KEMD.R "

--- R/KEMD.R
+++ R/KEMD.R
@@ -1,31 +1,3 @@
-
-cppFunction('
-  NumericVector computePeak(NumericVector BP, NumericVector peak)  {
-            int n = BP.size();
-            for(int i=1; i<n; i++) {
-            peak[i] = peak[i-1];
-            if (BP[i-1] > BP[i] && BP[i-1] > BP[i-2]){
-            peak[i] = BP[i-1];
-            }
-            }
-            return peak;
-            }        
-            ')
-
-
-cppFunction('
-            NumericVector computeValley(NumericVector BP, NumericVector valley)  {
-            int n = BP.size();
-            for(int i=1; i<n; i++) {
-            valley[i] = valley[i-1];
-            if (BP[i-1] < BP[i] && BP[i-1] < BP[i-2]){
-            valley[i] = BP[i-1];
-            }
-            }
-            return valley;
-            }        
-            ')
-
 #Cycle Amplitude Calculation
 #http://www.mesasoftware.com/Seminars/Trend%20Modes%20and%20Cycle%20Modes.pdf
 #'Kipnis Empirical Mode Decomposition
@@ -54,9 +26,12 @@ cppFunction('
   bandpass <- xts(filter(.5*(1-alpha)*(x-lag2x), c(beta*(1+alpha), -1*alpha), method="recursive"), order.by=index(x))
   bandpassSD <- runSD(bandpass, n) #this is Ilya's own modification. Bandpass SD is usually < 1 except in cases of trends.
   trend <- xts(maType(bandpass, n=2*n), order.by=index(x))
-  peak <- valley <- rep(0, length(bandpass))
-  peak <- xts(computePeak(bandpass, peak), order.by=index(x))
-  valley <- xts(computeValley(bandpass, valley), order.by=index(x))
+  peak <- valley <- rep(NA, length(bandpass))
+  peakIndex <- which(lag(bandpass) > bandpass & lag(bandpass) > lag(bandpass,2))-1
+  valleyIndex <- which(lag(bandpass) < bandpass & lag(bandpass) < lag(bandpass, 2))-1
+  peak[peakIndex] <- bandpass[peakIndex]; valley[valleyIndex] <- bandpass[valleyIndex]
+  peak <- xts(peak, order.by=index(x)); valley <- xts(valley, order.by=index(x))
+  peak <- na.locf(peak); valley <- na.locf(valley)
   meanPeak <- bandFraction*maType(peak, n=floor(2.5*n))/bandpassSD
   meanValley <- bandFraction*maType(valley, n=floor(2.5*n))/bandpassSD
   out <- cbind(trend, meanPeak, meanValley)
